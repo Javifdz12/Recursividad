13ea53638190934e54bf438d29742cdab42aafda
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-03-14 20:38:50 +0100

    gg

---
+++ ejercicio2.py
@@ -0,0 +1,56 @@
+import unicodedata
+import sys
+
+def palindromo(x):
+    x=x.lower()
+    x=x.replace(" ","")
+    caracteres=dict.fromkeys(c for c in range(sys.maxunicode) if unicodedata.combining(chr(c)))
+    x_normalizado=unicodedata.normalize("NFD",x)
+    x=x_normalizado.translate(caracteres)
+    print(x)
+    longitud=len(x)
+    if longitud%2==0:
+        izquierda=x[:longitud//2]
+        derecha=x[longitud//2:]
+        print(izquierda)
+        print(derecha)
+        return izquierda==derecha[::-1]
+    else:
+        izquierda=x[:longitud//2]
+        derecha=x[longitud//2+1:]
+        print(izquierda)
+        print(derecha)
+        return izquierda==derecha[::-1]
+
+
+print(palindromo("Atar a la rata"))
+
+def normalizar(x):
+    x=x.lower()
+    x=x.replace(" ","")
+    caracteres=dict.fromkeys(c for c in range(sys.maxunicode) if unicodedata.combining(chr(c)))
+    x_normalizado=unicodedata.normalize("NFD",x)
+    x=x_normalizado.translate(caracteres)
+    return x
+
+frase=str(input("escribe algo"))   
+frase=normalizar(frase)
+print(frase)
+
+def palindromo2(frase):
+    i = 0 
+    j = -1
+    a = True
+    if i >= len(frase):
+        if a == True:
+            if frase[i] != frase[j]:
+                a = False
+        i += 1
+        j -= 1
+        palindromo2(frase, i, j, a)
+
+    return a
+
+palindromo2(frase)
+
+
